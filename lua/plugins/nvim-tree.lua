7f529186100ed42488a54e359f08a3c3d7a6c2b5
David Su <js202@terminator8.cs.rice.edu>
2025-07-08 17:21:17 -0500

    Update.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,100 @@
+local config_func = function()
+    require("nvim-tree").setup({
+    disable_netrw = true,
+    hijack_netrw = true,
+    -- open_on_setup = true,
+    -- ignore_ft_on_setup = {
+    --     "startify",
+    --     "dashboard",
+    --     "alpha",
+    -- },
+    open_on_tab = false,
+    hijack_cursor = false,
+    update_cwd = true,
+    hijack_directories = {
+        enable = true,
+        auto_open = true,
+    },
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "H",
+            info = "I",
+            warning = "W",
+            error = "E",
+        },
+    },
+    -- update_focused_file = {
+    --     enable = true,
+    --     update_cwd = true,
+    --     ignore_list = {},
+    -- },
+    git = {
+        enable = true,
+        ignore = false,
+        timeout = 500,
+    },
+    view = {
+        width = {
+			min = 40,
+			max = 100,
+		},
+        side = "left",
+		centralize_selection = false,
+
+        -- mappings = {
+        --     custom_only = false,
+        --     list = {
+        --     { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        --     { key = "h", cb = tree_cb "close_node" },
+        --     { key = "v", cb = tree_cb "vsplit" },
+        --     },
+        -- },
+        number = true,
+        relativenumber = false,
+    },
+    actions = {
+		open_file = {
+			quit_on_open = false,
+			window_picker = { enable = true },
+		}
+    },
+    renderer = {
+        highlight_git = "all",
+        root_folder_modifier = ":t",
+        icons = {
+            show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = true,
+            },
+            glyphs = {
+                default = "",
+                symlink = "",
+                git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    deleted = "",
+                    untracked = "[U]",
+                    ignored = "◌",
+                },
+                folder = {
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                },
+            }
+        }
+    }
+})
+end
+
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
@@ -5,7 +102,5 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
+  config = config_func,
 }
