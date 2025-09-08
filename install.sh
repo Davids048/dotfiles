759575b41c34699829959713622e874cd076466e
Davids048 <david@DS96-M4.local>
2025-09-08 16:30:29 -0700

    Add vimrc.

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 DOTFILES_DIR="$HOME/dotfiles"
 
 ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
-ln -sf "$DOTFILES_DIR/nvim/" "$HOME/.config/nvim"
+ln -sfn "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
+ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
 
 
 echo "Dotfiles installed!"
