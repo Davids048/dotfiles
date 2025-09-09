4439aa3035264cd0cb6e3adac3df140f66579afa
Davids048 <david@DS96-M4.local>
2025-09-09 09:37:50 -0700

    Update

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,69 +1,69 @@
-local status_ok, lualine = pcall(require, "lualine")
-if not status_ok then
-    return
-end
-
-local hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
-end
-
-local diagnostics = {
-    "diagnostics",
-    sources = { "nvim_diagnostic" },
-    sections = { "error", "warn" },
-    symbols = { error = " ", warn = " " },
-    colored = false,
-    update_in_insert = false,
-    always_visible = true,
-}
-
-local diff = {
-    "diff",
-    colored = false,
-    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-  cond = hide_in_width
-}
-
-local mode = {
-    "mode",
-    fmt = function(str)
-        return "-- " .. str .. " --"
-    end,
-}
-
-local filetype = {
-    "filetype",
-    icons_enabled = false,
-    icon = nil,
-}
-
-local branch = {
-    "branch",
-    icons_enabled = true,
-    icon = "",
-}
-
-local location = {
-    "location",
-    padding = 0,
-}
-
--- cool function for progress
-local progress = function()
-    local current_line = vim.fn.line(".")
-    local total_lines = vim.fn.line("$")
-    local chars = { "██", "▇▇", "▆▆", "▅▅", "▄▄", "▃▃", "▂▂", "▁▁", "__"}
-    local line_ratio = current_line / total_lines
-    local index = math.ceil(line_ratio * #chars)
-    return chars[index]
-end
-
-local spaces = function()
-    return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-end
-
-
 local config_func = function()
+    local status_ok, lualine = pcall(require, "lualine")
+    if not status_ok then
+        return
+    end
+
+    local hide_in_width = function()
+        return vim.fn.winwidth(0) > 80
+    end
+
+    local diagnostics = {
+        "diagnostics",
+        sources = { "nvim_diagnostic" },
+        sections = { "error", "warn" },
+        symbols = { error = " ", warn = " " },
+        colored = false,
+        update_in_insert = false,
+        always_visible = true,
+    }
+
+    local diff = {
+        "diff",
+        colored = false,
+        symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+      cond = hide_in_width
+    }
+
+    local mode = {
+        "mode",
+        fmt = function(str)
+            return "-- " .. str .. " --"
+        end,
+    }
+
+    local filetype = {
+        "filetype",
+        icons_enabled = false,
+        icon = nil,
+    }
+
+    local branch = {
+        "branch",
+        icons_enabled = true,
+        icon = "",
+    }
+
+    local location = {
+        "location",
+        padding = 0,
+    }
+
+    -- cool function for progress
+    local progress = function()
+        local current_line = vim.fn.line(".")
+        local total_lines = vim.fn.line("$")
+        local chars = { "██", "▇▇", "▆▆", "▅▅", "▄▄", "▃▃", "▂▂", "▁▁", "__"}
+        local line_ratio = current_line / total_lines
+        local index = math.ceil(line_ratio * #chars)
+        return chars[index]
+    end
+
+    -- local spaces = function()
+    --     return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+    -- end
+
+
     lualine.setup({
         options = {
             icons_enabled = true,
@@ -83,8 +83,8 @@ local config_func = function()
                     path = 2,
                 }
             },
-            -- lualine_x = { "encoding", "fileformat", "filetype" },
-            lualine_x = { diff, spaces, "encoding", filetype },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            -- lualine_x = { diff, spaces, "encoding", filetype },
             lualine_y = { location },
             lualine_z = { progress },
         },
@@ -98,10 +98,13 @@ local config_func = function()
         },
         tabline = {},
         extensions = {},
-    })  
+    })
 
     end
 
+
+config_func = function ()
+end
 
 return {
     'nvim-lualine/lualine.nvim',
