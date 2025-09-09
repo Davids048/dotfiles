dcad541b3c3a92be3ee1922aff92ef62f2d67425
Davids048 <david@DS96-M4.local>
2025-09-09 09:28:52 -0700

    Bug fix: called module before loading.

--- nvim/lua/plugins/ufo-folding.lua
+++ nvim/lua/plugins/ufo-folding.lua
@@ -3,10 +3,6 @@ vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decr
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
--- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
 
 return {
     "kevinhwang91/nvim-ufo",
@@ -16,6 +12,9 @@ return {
             provider_selector = function(bufnr, filetype, buftype)
                 return {'treesitter', 'indent'}
             end
+            -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
         })
+        vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+        vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
     end
 }
