54f7d1ae8680a803d106342d513d257a4f6df524
Davids048 <david@DS96-M4.local>
2025-09-10 18:38:42 -0700

    update.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 DOTFILES_DIR="$HOME/dotfiles"
 
 ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/.bashrc" "$HOME/.bashrc"
 ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
 ln -sfn "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
