ab97b7c2901f91c40e0d889713588411d8218102
Davids048 <jundasu@ucsd.edu>
2025-09-14 18:12:03 +0000

    Update runpod setup.
    Include more default apps; install nodejs.

--- runpod-setup.sh
+++ runpod-setup.sh
@@ -2,8 +2,9 @@
 # Install softwares used on ssh servers. 
 
 apt update 
-apt install tmux vim -y
+apt install tmux vim npm nodejs tree -y
 
 wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz
 tar xf nvim-linux-x86_64.tar.gz --no-same-owner
-alias nvim="$(pwd)/nvim-linux-x86_64/bin/nvim"
+
+source nodejs.sh
