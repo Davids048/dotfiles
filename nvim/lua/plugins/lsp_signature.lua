59314e2183626b8423e1d9efd469d81df2cdaf04
Davids048 <jundasu@ucsd.edu>
2025-09-22 21:29:29 +0000

    Update lspsignature

--- nvim/lua/plugins/lsp_signature.lua
+++ nvim/lua/plugins/lsp_signature.lua
@@ -1,5 +1,15 @@
 local config_func = function()
-    require("lsp_signature").setup({})
+    require("lsp_signature").setup({
+      floating_window = false,
+      floating_window_off_x = 4,
+      hint_inline = function() return 'eol' end,
+      hint_prefix = {
+        above = "↙ ",  -- when the hint is on the line above the current line
+        current = "← ",  -- when the hint is on the same line
+        below = "↖ "  -- when the hint is on the line below the current line
+      },
+      toggle_key = '<C-k>'
+    })
 end
 
 return {
