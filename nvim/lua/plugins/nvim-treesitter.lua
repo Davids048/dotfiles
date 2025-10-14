0f99b248aeb764346a0580518f3c33fe9be99412
Davids048 <jundasu@ucsd.edu>
2025-10-14 02:43:11 +0000

    Update indent and keymaps.

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -15,7 +15,7 @@ local config_func = function()
 
       ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
       -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-      indent = { enable = true },
+      indent = { enable = false },
 
       highlight = {
         enable = true,
--- nvim/lua/user/lsp/settings/pyright.lua
+++ nvim/lua/user/lsp/settings/pyright.lua
@@ -1,3 +1,26 @@
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "python",
+  callback = function()
+    vim.opt_local.autoindent = true
+    vim.opt_local.smartindent = false
+    vim.opt_local.cindent = false
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.softtabstop = 4
+  end,
+})
+
+vim.g.python_indent = {
+  disable_parentheses_indenting = false,
+  closed_paren_align_last_line = false,
+  searchpair_timeout = 150,
+  continue = "shiftwidth()",
+  open_paren = "shiftwidth()",
+  nested_paren = "shiftwidth()",
+}
+
+
 return {
 	settings = {
 
