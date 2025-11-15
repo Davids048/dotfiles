b606edff065d78f29d5673d222244625f384f86f
Davids048 <jundasu@ucsd.edu>
2025-11-15 18:30:01 +0000

    Update indent marker.

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,6 +1,12 @@
 local config_func = function()
-    require("ibl").setup()
+    require("ibl").setup({
+        scope = { enabled = false },
+    })
     end
+
+
+vim.api.nvim_set_hl(0, "IblIndent", { fg = "#3a3a3a" })
+vim.api.nvim_set_hl(0, "IblScope", { fg = "#505050" })
 
 return {
     "lukas-reineke/indent-blankline.nvim",
@@ -9,5 +15,4 @@ return {
     ---@type ibl.config
     opts = {},
     config = config_func
-
 }
